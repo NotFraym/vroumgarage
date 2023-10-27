05ed0360ef03188cf9ea182547c37110020e5e16
NotFraym <galois.mathieu02@gmail.com>
2023-10-27 14:47:20 +1100

    maj fonctionnement (enfin)

--- login_process.php
+++ login_process.php
@@ -6,10 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $password = $_POST['password'];
 
     // Connexion à la base de données
-    $host = '      ';
-    $user = '      ';
-    $pass = '      ';
-    $dbname = '      ';
+    $host = '127.0.0.1';
+    $user = 'e5Mathieu';
+    $pass = 'tfg!DJ!7rpztY3xR';
+    $dbname = 'garage';
 
     $conn = new mysqli($host, $user, $pass, $dbname);
 
